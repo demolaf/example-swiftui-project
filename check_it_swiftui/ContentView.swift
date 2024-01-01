ff1188249f58827a45b8b73e9d2a5f6bbfb35918
Ademola Fadumo <demolafadumo@gmail.com>
2024-01-01 15:00:30 +0100

    Initial Commit

---
+++ check_it_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  check_it_swiftui
+//
+//  Created by Ademola Fadumo on 01/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ check_it_swiftui/check_it_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  check_it_swiftuiApp.swift
+//  check_it_swiftui
+//
+//  Created by Ademola Fadumo on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct check_it_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
